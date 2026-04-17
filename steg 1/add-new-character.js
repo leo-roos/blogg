fcc17c8581de8acda7f4d7679afd3c69af50bef2
Leo <leoroos88@gmail.com>
2026-04-17 18:56:58 +0200

    Add edit-character page and image edit support

--- steg 1/add-new-character.js
+++ steg 1/add-new-character.js
@@ -16,4 +16,15 @@ document.addEventListener("DOMContentLoaded", function() {
 
         window.location.pathname = "/character-selection.html";
     })
+
+    form.querySelector("input[name='file-upload']").addEventListener("change", function(event) {
+        const file = event.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function(e) {
+                document.querySelector("img#current-image").src = e.target.result;
+            }
+            reader.readAsDataURL(file);
+        }
+    })
 })
