45eaec28e09cbc8627a01577138c159c852a7663
Leo <232914920+leo-roos@users.noreply.github.com>
2026-05-21 19:18:36 +0200

    Add About page and GitHub repos integration

--- steg 2/eleventy.config.js
+++ steg 2/eleventy.config.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const characters = require("./assets/characters.json");
 const sassPlugin = require("@11tyrocks/eleventy-plugin-sass-lightningcss");
 
-module.exports = function(eleventyConfig) {
+module.exports = async function(eleventyConfig) {
     const watchFolders = [
         "./assets/characters.json",
         "./assets/Fonts",
@@ -13,6 +13,22 @@ module.exports = function(eleventyConfig) {
         const element = watchFolders[index];
         eleventyConfig.addPassthroughCopy(element);
         eleventyConfig.addWatchTarget(element);
+    }
+    eleventyConfig.addWatchTarget("./assets/scss");
+
+    const response = await fetch("https://api.github.com/users/leo-roos/repos?sort=updated");
+    const data = await response.json();
+    const repos = [];
+    for (const repo of data) {
+        if (repo.archived) continue;
+        repos.push({
+            name: repo.name,
+            description: repo.description,
+            url: repo.html_url,
+            stars: repo.stargazers_count,
+            language: repo.language,
+            updatedAt: repo.updated_at
+        });
     }
 
     function formatDate(date, showDate, showTime) {
@@ -44,9 +60,27 @@ module.exports = function(eleventyConfig) {
         return formatDate(date, true, true);
     });
 
+    eleventyConfig.addFilter("githubRelativeDate", (dateString) => {
+        const date = new Date(dateString);
+        const now = new Date();
+
+        const diffMs = now - date;
+        const diffDays = Math.floor(diffMs / (1000 * 60 * 60 * 24));
+
+        if (diffDays === 0) return "Today";
+        if (diffDays === 1) return "Yesterday";
+        if (diffDays < 30) return `${diffDays} days ago`;
+
+        return formatDate(date, true, false);
+    });
+
     eleventyConfig.addFilter("lastModified", (inputPath) => {
         const stats = fs.statSync(inputPath);
         return stats.mtime;
+    });
+
+    eleventyConfig.addCollection("repos", () => {
+        return repos;
     });
 
     eleventyConfig.addCollection("allTags", (collectionApi) => {
@@ -67,7 +101,7 @@ module.exports = function(eleventyConfig) {
         return [...tagSet];
     });
 
-    eleventyConfig.addCollection("characters", (collectionApi) => {
+    eleventyConfig.addCollection("characters", () => {
         const charactersSet = new Set();
         for (const character of characters) {
             charactersSet.add(character);
